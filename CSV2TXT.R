2f74995d090af5b2d7bf9accc96b1825ec56995d
ninakersten <nina@kersten.at>
2018-07-03 12:59:34 +0200

    geändert:       CSV2TXT.R

--- CSV2TXT.R
+++ CSV2TXT.R
@@ -1,28 +1,19 @@
 ####CSV to TXT-Parser####by Nina Kersten
 
 #This R-Parser converts a .csv-file of a RPMA-Output to a .txt-file for inferring a boolean network with TS2B
-#install.packages("feather")
-#library(feather)
-#install.packages("curl")
-#library(curl)
-#Troubleshooting: Enter into the terminal: sudo apt-get install libcurl4-openssl-dev
-#package "rio"contains the function 'convert'
-#install.packages("rio")
-#library(rio)
-#install.packages("dplyr")
-#library(dplyr)
-install.packages("tibble")
-library(tibble)
 
-#function(file.csv){
+#install.packages("ddR")
+#library(ddR)
+
+
+### Creating the 1. Stimuli-Dataset: "FGF1"###
 	#read-in the data, exlude SlideID, Antibody Name
 	newdata <- read.csv(file="/home/nina/Schreibtisch/Masterarbeit/DREAM8-Breast-Cancer/Training data/experimental/experimental/CSV/BT20_main.csv", header=FALSE, skip = 2, sep = ",", dec = ".", stringsAsFactors = FALSE)
 
 	#filter all measurements with the FGF1-stimuli
   #newdata2 <- dplyr::filter(newdata, newdata[,3] == "FGF1")
-  newdata2 <- subset(newdata, V4 == "0min" | V3 == "FGF1") # Filter for all "0min" and "FGF1" entries
-  end_CSV <- which(newdata2[,3] == "FGF1") # gibt alle positionen wieder wo ein "FGF1" steht
-  newdata3 <- newdata2[newdata2[1:end_CSV +1],] # nimmt nur den Teil auf dem data frame, der bin "FGF1" endet
+  #newdata2 <- subset(newdata, V4 == "0min" | V3 == "FGF1") # Filter for all "0min" and "FGF1" entries
+  newdata2 <- subset(newdata, V4 == "0min" | V3 == "Insulin") # Filter for all "0min" and "FGF1" entries
   #newdata2 <- dplyr::filter(newdata, newdata[,3] == "Insulin")
   #newdata2 <- dplyr::filter(newdata, newdata[,3] == "IGF1")
   #newdata2 <- dplyr::filter(newdata, newdata[,3] == "EGF")
@@ -30,6 +21,10 @@ library(tibble)
   #newdata2 <- dplyr::filter(newdata, newdata[,3] == "Serum")
   #newdata2 <- dplyr::filter(newdata, newdata[,3] == "NRG1")
   #newdata2 <- dplyr::filter(newdata, newdata[,3] == "PBS")
+  #end_CSV <- which(newdata2[,3] == "FGF1") # gibt alle positionen wieder wo ein "FGF1" steht
+  end_CSV <- which(newdata2[,3] == "Insulin") # gibt alle positionen wieder wo ein "FGF1" steht
+  newdata3 <- newdata2[newdata2[1:end_CSV +1],] # nimmt nur den Teil auf dem data frame, der bin "FGF1" endet
+
   newdata3 <- newdata[1,]	
   newdata2$V1 <- NULL
 	newdata2$V2 <- NULL
@@ -55,28 +50,35 @@ library(tibble)
 	names(newdata4)[1] <- "NON"
 	newdata4[newdata4 == "HUGO ID"] = "#NON"
 
-	split_newdata4 <- newdata4[,1:24]	# Hälfte der Tabelle um mit kleineren Datensatz zu üben.
+	split_newdata4 <- newdata4[,1:20]	# Hälfte der Tabelle um mit kleineren Datensatz zu üben.
 
 	#split_newdata5 <- add_row(split_newdata4,V5 = 1 , V6 = 3 , .before = 1)
 		
 	#split_newdata4$V5[[1]] #Returns the value at a certain position in a dataframe
-	split_newdata5 <- split_newdata4[rep(1:nrow(split_newdata4),each=2),] 
-	split_newdata5$NON[[1]] = "NON" #Replace "#NON" by "NON"
+	#split_newdata5 <- split_newdata4[rep(1:nrow(split_newdata4),each=2),] 
+	#split_newdata5$NON[[1]] = "NON" #Replace "#NON" by "NON"
 	
-	s <- LETTERS[seq(split_newdata5[1,2:ncol(split_newdata5)])]
 	
-	write.table(split_size, file = "/home/nina/Schreibtisch/Masterarbeit/Algorithmen/TS2B/BooleanModeling2post/examples/CSV2TXT.txt", quote = FALSE, row.names=FALSE, col.names = TRUE)
-	newdata3 <- read.table(file="/home/nina/Schreibtisch/Masterarbeit/Algorithmen/TS2B/BooleanModeling2post/examples/CSV2TXT.txt", fill= TRUE, header = T)
-	#colnames(newdata) <- c("NAN", "A", "B")
+  # Replace ID by "A-Z" in the first row of the data frame
+	a <- c("NON",LETTERS[seq(split_newdata4[1,2:ncol(split_newdata4)])])
+	
+	setwd("/home/nina/Schreibtisch/Masterarbeit/Algorithmen/TS2B/BooleanModeling2post/examples/")
+	write.table(a, file = "CSV2TXTOutput.txt", quote = FALSE, row.names=FALSE, col.names = FALSE)
+	newdata3 <- read.table(file="CSV2TXTOutput.txt", fill= TRUE, header = F)
+	
+	# add "NON" in first entry to get the same dimension as split_newdata5
+  #x <- t(newdata3)
+	# transpose the list and convert it into a dataframe 
+  converted_data <- data.frame(as.list(t(newdata3)))
+  # equalize the names of the rows
+  names(converted_data) = names (split_newdata4)
+  # check Equalization
+  names(converted_data) == names (split_newdata4)
+  # Merge the dataframe of the char-header with A-Z with the oroginal data, to get two header
+  Data <- rbind(converted_data,split_newdata4 )
+  #Data[Data == "110"] = "2"
 
-	# verkleinert die Tabelle auf nur 10 Einträge
-	#newdata[,11:48]<- list(NULL)
-
-	#Speichert neue CSV-Datei
-	#write.csv(newdata, '/home/nina/Schreibtisch/Masterarbeit/DREAM8-Breast-Cancer/Training data/experimental/experimental/CSV/BT20_main_edit.csv', row.names=FALSE, na="")
-  #Converts the .csv to a .tsv
-	#convert("/home/nina/Schreibtisch/Masterarbeit/DREAM8-Breast-Cancer/Training data/experimental/experimental/CSV/BT20_main.csv", "/home/nina/Schreibtisch/Masterarbeit/DREAM8-Breast-Cancer/Training data/experimental/experimental/CSV/BT20_main.tsv")
-
-	#converts .tsv to a .txt for the TS2B network generating tool
-#}
+  write.table(Data, file = "/home/nina/Schreibtisch/Masterarbeit/Algorithmen/TS2B/BooleanModeling2post/examples/CSV2TXT.txt", quote = FALSE, row.names=FALSE, col.names = FALSE)
+  
+  
 
