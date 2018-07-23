c3d05869aef5063ddac0eb9266c7ab12946e8369
ninakersten <35892094+ninakersten@users.noreply.github.com>
2018-07-23 15:38:46 +0200

    Add files via upload

--- Boolean2bnet.R
+++ Boolean2bnet.R
@@ -1,5 +1,7 @@
+###### Boolean Model (TS2B-output) to .bnet-file format-Parser####written by Nina Kersten 2018
+
+# R version 3.4.3
 #pip3 install praw
-# R version 3.4.3 is used, written by Nina Kersten 2018
 #install.packages("utils")
 #install.packages("rPython")
 #library (utils)
@@ -43,21 +45,79 @@ boolean2bnet2[boolean2bnet2 == "and"] = as.character("&")
 boolean2bnet2[boolean2bnet2 == "or"] = "|"
 boolean2bnet2[boolean2bnet2 == "not"] = as.character("!")
 boolean2bnet2[boolean2bnet2 == "="] = ","
-boolean2bnet2[boolean2bnet2 == as.character("A*")] = "A"
-boolean2bnet2[boolean2bnet2 == as.character("B*")] = "B"
-boolean2bnet2[boolean2bnet2 == as.character("C*")] = "C"
-boolean2bnet2[boolean2bnet2 == as.character("D*")] = "D"
-boolean2bnet2[boolean2bnet2 == as.character("E*")] = "E"
-boolean2bnet2[boolean2bnet2 == as.character("F*")] = "F"
-boolean2bnet2[boolean2bnet2 == as.character("G*")] = "G"
-boolean2bnet2[boolean2bnet2 == as.character("H*")] = "H"
-boolean2bnet2[boolean2bnet2 == as.character("I*")] = "I"
+
+#Ersetze alle Buchstaben durch den Knotennamen
+#Lese input-file.txt für TS2B als data frame ein
+name2node <- read.table("/home/nina/Schreibtisch/Masterarbeit/Algorithmen/TS2B/BooleanModeling2post/examples/CSV2TXTOutput.txt", fill= TRUE, header= FALSE, stringsAsFactors = FALSE, comment.char = "") 
+#name2node <- name2node[2,]
+boolean2bnet2[boolean2bnet2 == "A*"] = name2node[2,2]
+boolean2bnet2[boolean2bnet2 == "A"] = name2node[2,2]
+boolean2bnet2[boolean2bnet2 == "B*"] = name2node[2,3]
+boolean2bnet2[boolean2bnet2 == "B"] = name2node[2,3]
+boolean2bnet2[boolean2bnet2 == "C*"] = name2node[2,4]
+boolean2bnet2[boolean2bnet2 == "C"] = name2node[2,4]
+boolean2bnet2[boolean2bnet2 == "D*"] = name2node[2,5]
+boolean2bnet2[boolean2bnet2 == "D"] = name2node[2,5]
+boolean2bnet2[boolean2bnet2 == "E*"] = name2node[2,6]
+boolean2bnet2[boolean2bnet2 == "E"] = name2node[2,6]
+boolean2bnet2[boolean2bnet2 == "F*"] = name2node[2,7]
+boolean2bnet2[boolean2bnet2 == "F"] = name2node[2,7]
+boolean2bnet2[boolean2bnet2 == "G*"] = name2node[2,8]
+boolean2bnet2[boolean2bnet2 == "G"] = name2node[2,8]
+boolean2bnet2[boolean2bnet2 == "H*"] = name2node[2,9]
+boolean2bnet2[boolean2bnet2 == "H"] = name2node[2,9]
+boolean2bnet2[boolean2bnet2 == "I*"] = name2node[2,10]
+boolean2bnet2[boolean2bnet2 == "I"] = name2node[2,10]
+boolean2bnet2[boolean2bnet2 == "J"] = name2node[2,11]
+boolean2bnet2[boolean2bnet2 == "J*"] = name2node[2,11]
+boolean2bnet2[boolean2bnet2 == "K"] = name2node[2,12]
+boolean2bnet2[boolean2bnet2 == "K*"] = name2node[2,12]
+boolean2bnet2[boolean2bnet2 == "L"] = name2node[2,13]
+boolean2bnet2[boolean2bnet2 == "L*"] = name2node[2,13]
+boolean2bnet2[boolean2bnet2 == "M"] = name2node[2,14]
+boolean2bnet2[boolean2bnet2 == "M*"] = name2node[2,14]
+boolean2bnet2[boolean2bnet2 == "N"] = name2node[2,15]
+boolean2bnet2[boolean2bnet2 == "N*"] = name2node[2,15]
+boolean2bnet2[boolean2bnet2 == "O"] = name2node[2,16]
+boolean2bnet2[boolean2bnet2 == "O*"] = name2node[2,16]
+boolean2bnet2[boolean2bnet2 == "P"] = name2node[2,17]
+boolean2bnet2[boolean2bnet2 == "P*"] = name2node[2,17]
+boolean2bnet2[boolean2bnet2 == "Q"] = name2node[2,18]
+boolean2bnet2[boolean2bnet2 == "Q*"] = name2node[2,18]
+boolean2bnet2[boolean2bnet2 == "R"] = name2node[2,19]
+boolean2bnet2[boolean2bnet2 == "R*"] = name2node[2,19]
+boolean2bnet2[boolean2bnet2 == "S"] = name2node[2,20]
+boolean2bnet2[boolean2bnet2 == "S*"] = name2node[2,20]
+boolean2bnet2[boolean2bnet2 == "T*"] = name2node[2,21]
+boolean2bnet2[boolean2bnet2 == "T"] = name2node[2,21]
+boolean2bnet2[boolean2bnet2 == "U*"] = name2node[2,22]
+boolean2bnet2[boolean2bnet2 == "U"] = name2node[2,22]
+boolean2bnet2[boolean2bnet2 == "V*"] = name2node[2,23]
+boolean2bnet2[boolean2bnet2 == "V"] = name2node[2,23]
+boolean2bnet2[boolean2bnet2 == "W*"] = name2node[2,24]
+boolean2bnet2[boolean2bnet2 == "W"] = name2node[2,24]
+boolean2bnet2[boolean2bnet2 == "X*"] = name2node[2,25]
+boolean2bnet2[boolean2bnet2 == "X"] = name2node[2,25]
+boolean2bnet2[boolean2bnet2 == "Y"] = name2node[2,26]
+boolean2bnet2[boolean2bnet2 == "Y*"] = name2node[2,26]
+boolean2bnet2[boolean2bnet2 == "Z*"] = name2node[2,27]
+boolean2bnet2[boolean2bnet2 == "Z"] = name2node[2,27]
+
+new <- as.data.frame(sapply(boolean2bnet2,gsub,pattern="\\.",replacement="_"))
+
+#character <- as.data.frame(as.character(boolean2bnet2))
+#for boolean2bnet2 ("A":"Z"){
+#  if (boolean2bnet2 == "A"){
+#   boolean2bnet2 = names2node[,2:ncol]
+#  }
+#    print("error")
+#}
 #boolean2bnet2$V1 <- sub("^[^[:graph:]]", "", boolean2bnet2$V1)
 #boolean2bnet2$V1 <- sub("^[\\*-]", "", boolean2bnet2$V1)
 # Nach erster Spalte lexikographisch ordnen
 # Von oben nach unten Buchstaben lexikographisch ersetzen
-write.table(boolean2bnet2, file = "output2.bnet", quote = FALSE, row.names=FALSE, col.names = FALSE)
-boolean2bnet2 <- read.table("output2.bnet", fill= TRUE, header= FALSE, stringsAsFactors = FALSE)
+write.table(new, file = "output2.bnet", quote = FALSE, row.names=FALSE, col.names = FALSE)
+new2 <- read.table("output2.bnet", fill= TRUE, header= FALSE, stringsAsFactors = FALSE)
 
 
 
--- CSV2TXT.R
+++ CSV2TXT.R
@@ -3,7 +3,9 @@
 #This R-Parser converts a .csv-file of a RPMA-Output to a .txt-file for inferring a boolean network with TS2B
 
 #install.packages("ddR")
+#install.packages("plyr")
 #library(ddR)
+#library(plyr)
 
 
 ### Creating the 1. Stimuli-Dataset: "FGF1"###
@@ -13,17 +15,17 @@
 	#filter all measurements with the FGF1-stimuli
   #newdata2 <- dplyr::filter(newdata, newdata[,3] == "FGF1")
   #newdata2 <- subset(newdata, V4 == "0min" | V3 == "FGF1") # Filter for all "0min" and "FGF1" entries
-  newdata2 <- subset(newdata, V4 == "0min" | V3 == "Insulin") # Filter for all "0min" and "FGF1" entries
+  #newdata2 <- subset(newdata, V4 == "0min" | V3 == "Insulin") # Filter for all "0min" and "Insulin" entries
+  #newdata2 <- subset(newdata, V4 == "0min" | V3 == "IGF1")
+  #newdata2 <- subset(newdata, V4 == "0min" | V3 == "EGF")
+  #newdata2 <- subset(newdata, V4 == "0min" | V3 == "HGF")
+  #newdata2 <- subset(newdata, V4 == "0min" | V3 == "Serum")
+  #newdata2 <- subset(newdata, V4 == "0min" | V3 == "NRG1")
+  newdata2 <- subset(newdata, V4 == "0min" | V3 == "PBS")
   #newdata2 <- dplyr::filter(newdata, newdata[,3] == "Insulin")
-  #newdata2 <- dplyr::filter(newdata, newdata[,3] == "IGF1")
-  #newdata2 <- dplyr::filter(newdata, newdata[,3] == "EGF")
-  #newdata2 <- dplyr::filter(newdata, newdata[,3] == "HGF")
-  #newdata2 <- dplyr::filter(newdata, newdata[,3] == "Serum")
-  #newdata2 <- dplyr::filter(newdata, newdata[,3] == "NRG1")
-  #newdata2 <- dplyr::filter(newdata, newdata[,3] == "PBS")
   #end_CSV <- which(newdata2[,3] == "FGF1") # gibt alle positionen wieder wo ein "FGF1" steht
-  end_CSV <- which(newdata2[,3] == "Insulin") # gibt alle positionen wieder wo ein "FGF1" steht
-  newdata3 <- newdata2[newdata2[1:end_CSV +1],] # nimmt nur den Teil auf dem data frame, der bin "FGF1" endet
+  #end_CSV <- which(newdata2[,3] == "Insulin") # gibt alle positionen wieder wo ein "FGF1" steht
+  #newdata3 <- newdata2[newdata2[1:end_CSV +1],] # nimmt nur den Teil auf dem data frame, der bin "FGF1" endet
 
   newdata3 <- newdata[1,]	
   newdata2$V1 <- NULL
@@ -47,10 +49,10 @@
 	#Verbinde wieder newdata2 mit newdata3:
 	newdata4 <- rbind(newdata3, newdata2) 
 	
-	names(newdata4)[1] <- "NON"
+	#names(newdata4)[1] <- "NON"
 	newdata4[newdata4 == "HUGO ID"] = "#NON"
 
-	split_newdata4 <- newdata4[,1:20]	# Hälfte der Tabelle um mit kleineren Datensatz zu üben.
+	split_newdata4 <- newdata4[,1:20]	# Hälfte der Tabelle um mit kleineren Datensatz zu üben.Sonst gibt es zu viele Knoten und zu wenig Messpunkte.
 
 	#split_newdata5 <- add_row(split_newdata4,V5 = 1 , V6 = 3 , .before = 1)
 		
@@ -61,24 +63,40 @@
 	
   # Replace ID by "A-Z" in the first row of the data frame
 	a <- c("NON",LETTERS[seq(split_newdata4[1,2:ncol(split_newdata4)])])
+	b <- (paste(a , collapse =" "))
+	c <- as.data.frame(b)
+	d <- rbind.fill(c, split_newdata4)
+  d[1,2] <- as.character(d[1,1])
+  d$b <- NULL
+  d[is.na(d)] <- ""
 	
-	setwd("/home/nina/Schreibtisch/Masterarbeit/Algorithmen/TS2B/BooleanModeling2post/examples/")
-	write.table(a, file = "CSV2TXTOutput.txt", quote = FALSE, row.names=FALSE, col.names = FALSE)
-	newdata3 <- read.table(file="CSV2TXTOutput.txt", fill= TRUE, header = F)
+  setwd("/home/nina/Schreibtisch/Masterarbeit/Algorithmen/TS2B/BooleanModeling2post/examples/")
+	write.table(d, file = "CSV2TXTOutput.txt", quote = FALSE, row.names=FALSE, col.names = FALSE)
+	
+	
+	
+	
+	
+	#newdata5 <- read.table(file="CSV2TXTOutput.txt", fill= TRUE, header = F)
 	
 	# add "NON" in first entry to get the same dimension as split_newdata5
   #x <- t(newdata3)
 	# transpose the list and convert it into a dataframe 
-  converted_data <- data.frame(as.list(t(newdata3)))
+  #converted_data <- data.frame(as.list(t(newdata5)))
   # equalize the names of the rows
-  names(converted_data) = names (split_newdata4)
-  # check Equalization
-  names(converted_data) == names (split_newdata4)
-  # Merge the dataframe of the char-header with A-Z with the oroginal data, to get two header
-  Data <- rbind(converted_data,split_newdata4 )
-  #Data[Data == "110"] = "2"
+  #names(converted_data) = names (split_newdata4)
 
-  write.table(Data, file = "/home/nina/Schreibtisch/Masterarbeit/Algorithmen/TS2B/BooleanModeling2post/examples/CSV2TXT.txt", quote = FALSE, row.names=FALSE, col.names = FALSE)
+  #names(converted_data) == names (split_newdata4) # check Equalization
+  # Merge the dataframe of the char-header with A-Z with the oroginal data, to get two header
+  Data <- rbind(converted_data,split_newdata4)
+  
+
+  
+
+  #take all letters into the first [1,1] entry of the dataframe
+ 
+
+  write.table(Data2, file = "/home/nina/Schreibtisch/Masterarbeit/Algorithmen/TS2B/BooleanModeling2post/examples/CSV2TXTOutput.txt", quote = FALSE, row.names=FALSE, col.names = FALSE, sep = "\t")
   
   
 
