f8cc42d2c371b881f5335424caa7f2374ede1794
Nina K <35892094+ninakersten@users.noreply.github.com>
2018-08-01 15:38:42 +0200

    Add files via upload

---
+++ bnet2sif.R
@@ -0,0 +1,48 @@
+### bnet2sif.R ### written by Nina Kersten
+
+# In this script the realationship of the protein-protein-interactions are provided
+# The bnet-format shows the boolean rules for each node in the network
+# Here the priority of the boolean operators is "!"-> "&" -> "|" (strong -> weak)
+# e.g.(in .bnet): ProteinA* ,  ProteinB & !ProteinC | ProteinD
+# e.g.(in .sif):  ProteinB  1 ProteinA
+#                 ProteinC -1 ProteinA
+#                 ProteinD  1 ProteinA
+#Thus the AUROC-value can be calculated by DREAMTools
+
+#R version 3.4.3 (2017-11-30)
+
+setwd("/home/nina/Schreibtisch/Masterarbeit/Algorithmen/TS2B/BooleanModeling2post/")
+bnet_data <- read.table("output2.bnet", fill= TRUE, header= FALSE, stringsAsFactors = FALSE, sep = " ")
+
+proteinlist <- c(bnet_data[,1])
+bnet_subdata <- bnet_data[,3:ncol(bnet_data)]
+#index <- which(bnet_data == "EIF4EBP1_pT37_pT46", arr.ind=TRUE)
+
+mybiglist <- list()
+
+for (protein in proteinlist){
+  
+  indexlist <- which(bnet_subdata == protein, arr.ind=TRUE)
+  name <- paste(protein,":",sep=" ")
+  mybiglist[[name]] <- indexlist
+  #print(index)
+  
+  #dir.create("/BnetData")
+  #write.table(indexlist,paste(protein,sep = ".","txt"), sep="\t", quote = FALSE, row.names=FALSE, col.names = FALSE)
+}
+
+
+for (x in mybiglist){
+  #for(y in 1:nrow(mybiglist)){
+    print(x)
+    
+    #if (bnet_subdata[y,y-1] == "!"){
+      #print("Inactive")
+    #}else if(bnet_subdata[y,y-1] == "|"){
+      #print("Active")
+    #}
+  #}
+}
+
+
+
