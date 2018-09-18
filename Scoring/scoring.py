0693f0b2638eb7df61bc1d0512416b35f5dd5202
ninakersten <nina@kersten.at>
2018-09-18 15:04:39 +0200

    geändert:       Scoring/scoring.py

--- Scoring/scoring.py
+++ Scoring/scoring.py
@@ -4,11 +4,8 @@ import itertools
 import collections
 import ast
 
-
-
 from collections import OrderedDict
 from itertools import chain
-
 
 
 #Read in the names_lists for the cartesian product
@@ -41,44 +38,26 @@ names_list = []
 for i in itertools.product(names_list1,names_list2):
 	#print(i)
 	names_list.append(i)
-	names_list2 = []
-	for z in names_list:
-		names_list2.append((''.join([w+' ' for w in z])).strip())
-		names_list2 = [x.replace(' ','') for x in names_list2]
-	
-print(names_list2)
+	#names_list2 = []
+	#for z in names_list:
+	#	names_list2.append((''.join([w+' ' for w in z])).strip())
+	#names_list2 = [x.replace(' ','') for x in names_list]
 
-
+print(names_list)
 
 line1 = gold_file.readlines()
 line2 = pred_file.readlines()
 
-tup_list = []
+
+#Calculating True Positive
+tp_biglist = []
 for gold in line1:
-	#print (gold)
-	for pred in line2:
-		#print(pred)
-		#Calculating True Positive
-		if pred == gold:
-			#Speicher die tp-Fälle als Tupel
-			tp_list = (pred,gold)
-			#print(tp_list)
-
-			tp_list1 = list(tp_list)
-
-			tp_list2 = [x.replace('\n','').replace('1','').replace(',','') for x in tp_list1]
-
-			tp_list4 = list(OrderedDict.fromkeys(tp_list2))
-
-			tup_li = [x.replace(' ','') for x in tp_list4]
-
-			tup_list.append(tup_li)
-
-
-
-new_list = list(chain(*tup_list))
-tp = len(new_list)
-
+	if gold in line2:
+		tp_list = list(gold)
+		tp_list = [e for e in tp_list if e not in (' ', '1','\n')]
+		tp_biglist.append(tuple(tp_list))
+		tp_biglist = [t for t in tp_biglist if t != ()]
+		tp = len(tp_biglist)
 
 
 #Calculating False Negative
@@ -86,9 +65,9 @@ fn_biglist = []
 for gold in line1:
 	if gold not in line2:
 		fn_list = list(gold)
-		#print(fn_list)
 		fn_list = [e for e in fn_list if e not in (' ', '1','\n')]
 		fn_biglist.append(tuple(fn_list))
+		fn_biglist = [t for t in fn_biglist if t != ()]
 		fn = len(fn_biglist)
 
 		
@@ -98,29 +77,34 @@ fp_biglist = []
 for pred in line2:
 	if pred not in line1:
 		fp_list = list(pred)
-	
 		fp_list = [e for e in fp_list if e not in (' ', '1','\n')]
-		#print(fp_list)
 		fp_biglist.append(tuple(fp_list))
-		
 		fp_biglist = [t for t in fp_biglist if t != ()]
-
 		fp = len(fp_biglist)
 
 
-
-
 #Deleting TP, FN, FP from the names_list2 to get finally the TN-value
-for string in new_list:
-	if string in names_list2:
-		names_list2.remove(string)
-		tn = len(names_list2)
+for tp_string in tp_biglist:
+		if tp_string in names_list:
+			names_list.remove(tp_string)
 
 
+for fp_string in fp_biglist:
+	if fp_string in names_list:
+		names_list.remove(fp_string)
+
+for fn_string in fn_biglist:
+	if fn_string in names_list:
+		names_list.remove(fn_string)
+
+		tn = len(names_list)
+
+
+print("True Negative Value =",tn)
 print("False Positive Value =",fp)
 print("True Positive Value =",tp)
 print("False Negative Value =",fn)
-print("True Negative Value =",tn)
+
 
 #Berechnung von Accuracy, Prcision und Recall
 acc = ((tp+tn)/(tp+tn+fp+fn))
