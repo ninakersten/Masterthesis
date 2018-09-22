39ae047285fb042c490da198d45392c37a173105
ninakersten <nina@kersten.at>
2018-09-22 16:32:49 +0200

    geändert:       Scoring/example/goldstandard.sif 	geändert:       Scoring/example/prediction.sif 	geändert:       Scoring/scoring.py

--- Scoring/scoring.py
+++ Scoring/scoring.py
@@ -1,8 +1,17 @@
-##Scoring program for calculating Precision, Recall and Accuracy of a Goldstandard network against it's Prediction####writte by Nina Kersten 2018
+#Scoring program for calculating Accuracy, Precision and Recall, written by Nina Kersten#
+#This program runs an example for real data
+
+
 import sys,os, re
 import csv
 import itertools
+import ast
+import math
 
+
+#Read in the names_lists for the cartesian product
+#names_list1 = ["4EBP1_pS65","4EBP1_pT37_pT46","ACC_pS79","AKT_pS473","AKT_pT308"]
+#names_list2 = ["4EBP1_pS65","4EBP1_pT37_pT46","ACC_pS79","AKT_pS473","AKT_pT308"]
 
 #Importing a .csv file for a certain cellline, getting the names of the proteins
 rows = csv.reader(open("./example/BT20_main.csv", "r"), delimiter=',')
@@ -11,70 +20,66 @@ arows1= arows[0]
 arows2 = [x for x in arows1 if x]
 arows2.remove("Antibody Name")
 
+
 names_list1 = arows2 
 names_list2 = arows2
 
-#Creating a list of tuples of all possible combinations of the names
+#Create the cartesian productof the names
 names_list = []
 for i in itertools.product(names_list1,names_list2):
-	#print(i)
 	names_list.append(i)
-#print(len(names_list))
+
 
 #Converting the "\t"-seperated Goldstandard network into a whitespace seperated network
-with open("./realdata_example/Goldstandard.sif","r") as fin, open ("./realdata_example/Goldstandard1.sif","w") as fout:
+with open("./example/goldstandard.sif","r") as fin, open ("./example/goldstandard1.sif","w") as fout:
 	for line in fin:
 		fout.write(line.replace('\t',' '))
 #print(fout)
 
-
-
 #Read in the Golstandard and the Prediction data set
-gold_file = open("./realdata_example/Goldstandard1.sif","r")
-pred_file = open("./realdata_example/Prediction.sif","r")
-
+gold_file = open("./example/goldstandard1.sif","r")
+pred_file = open("./example/prediction.sif","r")
 
 
 line1 = gold_file.readlines()
 line2 = pred_file.readlines()
-#print(line1)
+#print(line1,line2)
 
-line1 = [elem.replace('\n','').replace('1, -1','1').replace('-1','1') for elem in line1]
-line2 = [elem.replace('\n','').replace('1, -1','1').replace('-1','1') for elem in line2]
-#print(line2)
+line1 = [elem.replace('\n','').replace(' 1, -1 ',' 1 ').replace(' -1 ',' 1 ') for elem in line1]
+line2 = [elem.replace('\n','').replace(' 1, -1 ',' 1 ').replace(' -1 ',' 1 ') for elem in line2]
+print(line1,line2)
+
+line1 = [e for e in line1 if e not in ('')]
+line2 = [e for e in line2 if e not in ('')]
+#print(line1, line2)
 
 #Calculating True Positive
 tp_biglist = []
+fn_biglist = []
 for gold in line1:
 	if gold in line2:
 		tp_list = list(gold)
+		#print(tp_biglist)
 		tp_list = ''.join(gold)
-		#print(tp_list)
+		#print(tp_biglist)
 		tp_list = tp_list.strip().split()
-		tp_list = [e for e in tp_list if e not in (' ', '1')]
+		tp_list = [e for e in tp_list if e not in ('1')]
 		tp_biglist.append(tuple(tp_list))
-		tp_biglist = [t for t in tp_biglist if t != ()]
+		#print(tp_biglist)
 		tp = len(tp_biglist)
-	else:
-		tp = 0
-
-#print(tp)
-
+		#print(tp_biglist) #[('a1a', 'b2b'), ('a1a', 'c3c'), ('e5e', 'a1a')]
 
 #Calculating False Negative
-fn_biglist = []
-for gold in line1:
-	if gold not in line2:
+	elif gold not in line2:
 		fn_list = list(gold)
 		fn_list = ''.join(gold)
 		fn_list = fn_list.strip().split()
-		fn_list = [e for e in fn_list if e not in (' ', '1')]
+		fn_list = [e for e in fn_list if e not in ('1')]
 		fn_biglist.append(tuple(fn_list))
+		#fn_biglist = [t for t in fn_biglist if t != ()]
 		fn = len(fn_biglist)
-	#else:
-	#	tn = 0
-#print(fn_biglist)
 
+#print(tp_biglist)		
 
 #Calculating False Positive
 fp_biglist = []
@@ -83,38 +88,62 @@ for pred in line2:
 		fp_list = list(pred)
 		fp_list = ''.join(pred)
 		fp_list = fp_list.strip().split()
-		fp_list = [e for e in fp_list if e not in (' ', '1')]
+		fp_list = [e for e in fp_list if e not in ('1')]
 		fp_biglist.append(tuple(fp_list))
-		fp_biglist = [t for t in fp_biglist if t != ()]
+		#fp_biglist = [t for t in fp_biglist if t != ()]
 		fp = len(fp_biglist)
-	#else:
-	#	fp = 0
 
 
-#Calculating the True Negative value by deleting the name pairs from the list of name combinations created above
-for tp_string in tp_biglist:
-	if tp_string in names_list:
-		names_list.remove(tp_string)
-	elif not tp_biglist:
-		break
+#Deleting TP, FN, FP from the names_list2 to get finally the TN-value
+def EmptyList1(tp_biglist):
+	if len(tp_biglist) == 0:
+		return 0
+	else:
+		return 1
+           
+if EmptyList1(tp_biglist): 
+	for tp_string in tp_biglist:
+		if tp_string in names_list:
+			names_list.remove(tp_string)
+else:
+	tp = 0
 
 
+print(len(names_list))
 
-for fp_string in fp_biglist:
-	if fp_string in names_list:
-		names_list.remove(fp_string)
-	#elif not fp_biglist:
-	#	break
+def EmpytList2(fp_biglist):
+	if len(fp_biglist) == 0:
+		return 0
+	else:
+		return 1
 
+if EmpytList2(fp_biglist): 
+	for fp_string in fp_biglist:
+		if fp_string in names_list:
+			names_list.remove(fp_string)
+else:
+	fp = 0
 
-for fn_string in fn_biglist:
-	if fn_string in names_list:
-		names_list.remove(fn_string)
-	#elif not fn_biglist:
-	#	break
+print(len(names_list))
 
+def EmpytList3(fn_biglist):
+	if len(fn_biglist) == 0:
+		return 0
+	else:
+		return 1
 
-		tn = len(names_list)
+if EmpytList3(fn_biglist): 
+	for fn_string in fn_biglist:
+		if fn_string in names_list:
+			names_list.remove(fn_string)
+else:
+	fn = 0
+
+print(len(names_list))
+		
+
+tn = len(names_list)
+
 
 print("True Negative Value =",tn)
 print("False Positive Value =",fp)
@@ -122,10 +151,19 @@ print("True Positive Value =",tp)
 print("False Negative Value =",fn)
 
 
-#Calculating the Accuracy, Recall and Precision
+#Berechnung von Accuracy, Prcision und Recall
 acc = ((tp+tn)/(tp+tn+fp+fn))
 print("Accuracy =",acc)
 pre = ((tp)/(tp+fp))
 print("Precision =",pre)
 rec = ((tp)/(tp+fn))
 print("Recall =",rec)
+
+#Calculating Matthiews correlation coefficient(MCC):
+mcc = ((tp*tn-fp*fn)/(math.sqrt((tp+fp)*(tp+fn)*(tn+fp)*(tn+fn))))
+print("Matthiews correlation coefficient (MCC) = ",mcc)
+
+#Balanced  Accuracy
+bacc = (((tp/(tp+fn))+(tn/(fp+tn)))/2)
+print("Balanced Accuracy =",bacc)
+
